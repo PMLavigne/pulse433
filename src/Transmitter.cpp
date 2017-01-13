76bcd81a95bdb068e46340512beb3d96b321d222
Patrick Lavigne <plavigne@madmobile.com>
2017-01-13 10:42:27 -0500

    EditorConfig and Transmitter class
    - Add .editorconfig file to maintain style
    - Initial commit of Transmitter class

---
+++ src/Transmitter.cpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include "Transmitter.h"
+
+namespace Pulse433 {
+
+};
---
+++ src/Transmitter.h
@@ -0,0 +1,11 @@
+#ifndef PULSE433__H
+#define PULSE433__H
+
+#include <Arduino.h>
+
+namespace Pulse433 {
+  class Transmitter {
+  };
+}
+
+#endif
