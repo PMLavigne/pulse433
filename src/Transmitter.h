70e72f666450974681103658a7108ee87821eba7
Patrick Lavigne <plavigne@madmobile.com>
2017-01-13 13:08:21 -0500

    Progress Commit
    - Add warning flags to build in library.json for compiler diagnostics
    - Add base pulse433.h header for platformio automatic detection
    - Fleshed out Transmitter class with encoder logic and state

--- src/Transmitter.h
+++ src/Transmitter.h
@@ -1,11 +1,46 @@
-#ifndef PULSE433__H
-#define PULSE433__H
+#ifndef PULSE433_TRANSMITTER_H
+#define PULSE433_TRANSMITTER_H
 
-#include <Arduino.h>
+#define PULSE433_TRANSMITTER_BUFF_SIZE 32
+#define PULSE433_TRANSMITTER_BITS_PER_BYTE 8
+
+#define PULSE433_TRANSMITTER_TICKS_PER_SHORT_PULSE 1
+#define PULSE433_TRANSMITTER_TICKS_PER_LONG_PULSE 4
+#define PULSE433_TRANSMITTER_TICKS_PER_START_PULSE 8
 
 namespace Pulse433 {
-  class Transmitter {
-  };
+
+    class Transmitter {
+    private:
+
+        enum class TransmissionState {
+            Off,
+            Start,
+            Zero,
+            One,
+            Stop
+        } state;
+
+        void (*transmitFunction)(bool);
+
+        unsigned char tickCount = 0;
+
+        unsigned char buff[PULSE433_TRANSMITTER_BUFF_SIZE];
+        unsigned char buffPos = 0;
+        unsigned char buffBytePos = 0;
+        unsigned char buffLength = 0;
+
+        void nextState();
+
+    public:
+        Transmitter(void (*transmitFunction)(bool)) : transmitFunction(transmitFunction) {
+            this->reset();
+        }
+
+        bool isBusy() const;
+        void reset();
+        void tick();
+    };
 }
 
 #endif
---
+++ src/pulse433.h
@@ -0,0 +1,6 @@
+#ifndef PULSE433_H
+#define PULSE433_H
+
+#include <Transmitter.h>
+
+#endif
